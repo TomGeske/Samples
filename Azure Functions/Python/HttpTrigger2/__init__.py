f8798aa661518f3e39acad8cdbf940788a912698
Thomas Geske <thomasge@microsoft.com>
2021-08-10 12:30:02 +0200

    added second test function

--- Azure Functions/Python/HttpTrigger2/__init__.py
+++ Azure Functions/Python/HttpTrigger2/__init__.py
@@ -1,22 +1,22 @@
 import logging
 
 import azure.functions as func
-
+import requests
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
-    name = req.params.get('name')
-    if not name:
+    response = req.params.get('response')
+    if not response:
         try:
-            req_body = req.get_json()
+            response = requests.get("https://github.com/timeline.json")
         except ValueError:
             pass
         else:
-            name = req_body.get('name')
+            responseContent = response.json() 
 
-    if name:
-        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
+    if responseContent:
+        return func.HttpResponse(f"API Result: {responseContent}")
     else:
         return func.HttpResponse(
              "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
